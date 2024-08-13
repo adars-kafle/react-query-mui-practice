1f3cb8b85c2b4e4ee42d82a6ba032be73dc7130d
adars124 <lonleyau@gmail.com>
2024-08-13 13:47:33 +0545

    fixed routes and main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -12,3 +12,32 @@ import MainRoutes from "./routes";
 // Toaster and it's css
 import { ToastContainer, Flip } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const queryClient = new QueryClient();
+const rootElement = document.getElementById("root");
+
+const App = () => {
+  return (
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <QueryClientProvider client={queryClient}>
+          <MainRoutes />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            newestOnTop
+            closeOnClick
+            theme="colored"
+            transition={Flip}
+          />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </StrictMode>
+  );
+};
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,1 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const MainRoutes = () => {
+  return (
+    <>
+      <div>Main route section!</div>
+    </>
+  );
+};
+
+export default MainRoutes;
