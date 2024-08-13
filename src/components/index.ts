5f934afe3c343176a04327393ff8930a00813609
adars124 <lonleyau@gmail.com>
2024-08-13 14:31:43 +0545

    added default export for components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Layout from "./Layout/Layout";
+import * as Task from "./TaskComponents";
+import EditProfile from "./EditProfile";
+import ProtectedRoute from "./ProtectedRoute";
+
+export { Layout, Task, EditProfile, ProtectedRoute };
