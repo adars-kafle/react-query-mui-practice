c376ed5f6851ea074362a44db8225cd7c8cd988f
adars124 <lonleyau@gmail.com>
2024-08-14 12:12:35 +0545

    added completed tasks component

--- src/components/TaskComponents/CompletedTasks.tsx
+++ src/components/TaskComponents/CompletedTasks.tsx
@@ -1,5 +1,36 @@
-const CompletedTasks = () => {
-  return <div>CompletedTasks</div>;
-};
+import { useTaskContext } from "../../context/TaskContext";
+import TaskItem from "./TaskItem";
+import { Box, Paper, Grid, Container, Typography } from "@mui/material";
 
-export default CompletedTasks;
+export default function CompletedTasks() {
+  const { tasks } = useTaskContext();
+  const completedTasks = tasks.filter((task) => task.status === "completed");
+
+  // console.log("completedTasks", completedTasks);
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Completed Tasks
+      </Typography>
+      <Box
+        component={Paper}
+        elevation={3}
+        sx={{
+          borderRadius: 2,
+          width: "full",
+          minHeight: "450px",
+          padding: 1,
+          backgroundColor: "#fafafa",
+        }}
+      >
+        <Grid container rowSpacing={1} columnSpacing={2}>
+          {completedTasks.map((task) => (
+            <Grid item xs={12} sm={6} key={task._id}>
+              <TaskItem task={task} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
