0046f62d0efec255f17458ecff9195196e1ef5d0
adars124 <lonleyau@gmail.com>
2024-08-14 12:01:15 +0545

    added all tasks

--- src/components/TaskComponents/AllTasks.tsx
+++ src/components/TaskComponents/AllTasks.tsx
@@ -1,5 +1,31 @@
+import { useTaskContext } from "../../context/TaskContext";
+import { Container, Grid, Typography } from "@mui/material";
+import TaskContainer from "./AllTasksTaskContainer";
+import { type Task } from "../../types";
+
 const AllTasks = () => {
-  return <div>AllTasks</div>;
+  const { tasks } = useTaskContext();
+
+  const statuses = ["pending", "in-progress", "completed"];
+
+  const getTasksByStatus = (status: string) => {
+    return tasks.filter((task: Task) => task.status === status);
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        All Tasks
+      </Typography>
+      <Grid container spacing={3}>
+        {statuses.map((status) => (
+          <Grid item xs={12} md={4} key={status}>
+            <TaskContainer status={status} tasks={getTasksByStatus(status)} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default AllTasks;
