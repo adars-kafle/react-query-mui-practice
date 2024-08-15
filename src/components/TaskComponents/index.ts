2e77d034b6d7f384a94f278c8eb1a79b31a14fe7
adars124 <lonleyau@gmail.com>
2024-08-15 12:27:45 +0545

    resolved exports

--- src/components/TaskComponents/index.ts
+++ src/components/TaskComponents/index.ts
@@ -1,6 +1,4 @@
-import AllTasks from "./AllTasks";
-import CompletedTasks from "./CompletedTasks";
-import InProgressTasks from "./InProgressTasks";
-import PendingTasks from "./PendingTasks";
-
-export { AllTasks, CompletedTasks, InProgressTasks, PendingTasks };
+export * from "./AllTasks";
+export * from "./CompletedTasks";
+export * from "./InProgressTasks";
+export * from "./PendingTasks";
